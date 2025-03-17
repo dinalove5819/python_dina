b0c08595981d229cea3de755b82616cf1fad55dd
dinalove5819 <jenniferny0330@gmail.com>
2025-03-17 23:25:51 +0900

    inputTest_250317.py

--- inputTest_250317.py
+++ inputTest_250317.py
@@ -1,1 +1,22 @@
-print("hello world")
+inputValue=input("숫자 ==>")
+print(inputValue)
+num1=input("숫자1 ==>")
+print(num1)
+num2=input("숫자2 ==>")
+print(num2)
+
+num1 = 100
+num2 = 10
+
+result = num1 + num2
+print(num1,"+",num2,"=",result)
+result= num1-num2
+print(num1,"-",num2,"=",result)
+result= num1*num2
+print(num1,"*",num2,"=",result)
+result=num1/num2
+print(num1,"/",num2,"=",result)
+result=num1%num2
+print(num1,"%",num2,"=",result)
+result=num1**num2
+print(num1,"**",num2,"=",result)
