0e2dfd09e068383968ed47d84a1baf36eb47b55e
Kwak Nayoung <jenniferny0330@gmail.com>
2025-03-31 11:55:33 +0900

    main

--- CanvasTest.py
+++ CanvasTest.py
@@ -1,6 +1,9 @@
 import tkinter
 root = tkinter.Tk()
 root.title("캔버스 만들기")
-canvas = tkinter.Canvas(root, width=400, height=600, bg="skyblue")
+canvas = tkinter.Canvas(root, width=400, height=600, bg="#c4bee2")
+
+bgimg=tkinter.PhotoImage(file="miko.png")
+
 canvas.pack()
 root.mainloop()
