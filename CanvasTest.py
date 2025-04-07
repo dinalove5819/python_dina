42af8d867137926edbfa9b3527b6a0e48dfd3ed5
Kwak Nayoung <jenniferny0330@gmail.com>
2025-04-07 10:33:24 +0900

    main

--- CanvasTest.py
+++ CanvasTest.py
@@ -10,5 +10,19 @@ canvas.pack()
 bgimg=tkinter.PhotoImage(file="miko.png")
 canvas.create_image(400,300,image=bgimg)
 
+#좌표 출력기
+def mouseMove(event):
+    x = event.x
+    y = event.y
+    labelMouse["text"]=str(x)+","+str(y)
+
+#캔버스 생성
+canvas = tkinter.Canvas(root, width=400, height=600, bg="#c4bee2")
+canvas.pack()
+
+#좌표 출력기
+root.bind("<Motion>", mouseMove)
+labelMouse=tkinter.Label(root,text=",",font=("맑은고딕",10))
+labelMouse.pack()
 
 root.mainloop()
