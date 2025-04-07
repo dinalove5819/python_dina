88ecf5eff29aef28cdf648de5486a2f5d4f2eec7
Kwak Nayoung <jenniferny0330@gmail.com>
2025-04-07 10:53:03 +0900

    main

--- CanvasTest.py
+++ CanvasTest.py
@@ -1,20 +1,17 @@
 import tkinter
+import random
+
 root = tkinter.Tk()
 root.title("캔버스 만들기")
-
-#캔버스 생성
-canvas = tkinter.Canvas(root, width=400, height=600, bg="#c4bee2")
-canvas.pack()
-
-#캔버스 내 이미지 생성
-bgimg=tkinter.PhotoImage(file="miko.png")
-canvas.create_image(400,300,image=bgimg)
 
 #좌표 출력기
 def mouseMove(event):
     x = event.x
     y = event.y
     labelMouse["text"]=str(x)+","+str(y)
+
+def click_btn():
+    label["text"]=random.choice(["대길","중길","소길","흉"])
 
 #캔버스 생성
 canvas = tkinter.Canvas(root, width=400, height=600, bg="#c4bee2")
@@ -24,5 +21,15 @@ canvas.pack()
 root.bind("<Motion>", mouseMove)
 labelMouse=tkinter.Label(root,text=",",font=("맑은고딕",10))
 labelMouse.pack()
+
+#캔버스 내 이미지 생성
+bgimg=tkinter.PhotoImage(file="miko.png")
+canvas.create_image(400,300,image=bgimg)
+
+label = tkinter.Label(root,text=",",font=("맑은고딕",120))
+label.place(x=380, y=60)
+
+button = tkinter.Button(root,text="제비뽑기",font=("맑은 고딕",36),command=click_btn)
+button.place(x=360, y=400)
 
 root.mainloop()
